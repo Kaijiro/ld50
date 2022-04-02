83cc7b5259b482b2727280cffb8800af19f0c473
OkkoLOBR <olivier.kowalczyk@gmail.com>
2022-04-02 18:10:20 +0200

    Add loading system, and improve aiming feeling

--- Assets/Scripts/ShootingRangeStudy.cs
+++ Assets/Scripts/ShootingRangeStudy.cs
@@ -1,35 +1,64 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShootingRangeStudy : MonoBehaviour
 {
+    public int shoot_capacity = 1;
+    private float shoot_left = 1f;
+    public Slider shoot_loader;
+    public float loading_velocity = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        shoot_left = shoot_capacity;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) )
         {
-            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
-
-            if (hit.collider != null)
+           if( shoot_left >= 1)
             {
-                Debug.Log("Clicked on " + hit.collider.name);
+                shoot_left--;
+                shoot_loader.value = shoot_left/shoot_capacity;
 
-                Destroy(hit.collider.gameObject);
-            }
-            else
+                Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+
+                if (hit.collider != null)
+                {
+                    Debug.Log("Clicked on " + hit.collider.name);
+
+                    Destroy(hit.collider.gameObject);
+                }
+                else
+                {
+                    Debug.Log("Nothing hit");
+                }
+            } else
             {
-                Debug.Log("Nothing hit");
+                Debug.Log("RELOAD");
             }
+            
+        }      
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (mousePosition.y < -5f)
+        {
+            shoot_left = Mathf.Min(shoot_capacity, shoot_left + Time.deltaTime * loading_velocity);
+            //m_Rigidbody.MovePosition(transform.position + m_Input * Time.deltaTime * m_Speed);
         }
+
+        shoot_loader.value = shoot_left / shoot_capacity;
     }
 
 }
--- Assets/Scripts/theHand.cs
+++ Assets/Scripts/theHand.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class theHand : MonoBehaviour
 {
+
     void Update()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = Camera.main.transform.position.z + Camera.main.nearClipPlane;
-        mousePosition.x += 1f;
-        mousePosition.y -= 1.8f;
         transform.position = mousePosition;
-  
+           
     }
 }
